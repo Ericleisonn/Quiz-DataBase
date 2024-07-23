45e55a223cd79d4fe3a06c33d80102f3882b463d
Anderson Azevedo <andersonsilva14.2017@gmail.com>
2024-07-23 09:30:57 -0300

    up

--- quiz-database/pages/components/nav.js
+++ quiz-database/pages/components/nav.js
@@ -2,13 +2,17 @@ import styles from '../../styles/nav.module.css'
 
 export default function Nav(){
     return (
-        <>
-            <div className={styles.nav}>
-                <nav className="navbar">
+        <>  
+            <link href="https://fonts.googleapis.com/css2?family=Jura:wght@300..700&display=swap" rel="stylesheet"></link>
+            <link href="https://fonts.googleapis.com/css2?family=Jura:wght@300..700&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet"></link>
+
+            <div>
+                <nav className="d-flex justify-content-between" id={styles.nav}>
                     <img src='logo.png'/>
                     <div className={styles.quadrado}>
-                        <h1>Simulado de questões (Quiz DATA-Base)</h1>
+                        <h1>Simulado de questões <br/> (Quiz DATA-Base)</h1>
                     </div>
+                    <button type="button" id={styles.button_login} class="btn">Login</button>
                 </nav>
             </div>
         </>
--- quiz-database/pages/index.js
+++ quiz-database/pages/index.js
@@ -1,6 +1,7 @@
 export default function Home(){
     return(
         <main>
+            <title>Quiz</title>
             <div>
                 <h1>Bem-Vindo ao Quiz-DataBase</h1>
             </div>
